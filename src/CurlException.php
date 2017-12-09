8b50aed3d28cbb56d6ef287b28fa334b7fe6539c
Sergey Latyshev <s.latyshev@xsolla.com>
2017-12-09 21:09:06 +0500

    Hugest fix ever

--- src/CurlException.php
+++ src/CurlException.php
@@ -9,7 +9,7 @@ class CurlException extends \RuntimeException
     public function __construct($logInfo, $curlError, $code = 0, Throwable $previous = null)
     {
         $message = $logInfo . PHP_EOL;
-        $message .= 'Curl error:' . $curlError;
+        $message .= 'Curl error: ' . $curlError;
         parent::__construct($message, $code, $previous);
     }
 }
